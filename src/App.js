06279a57fecb408954cc8980dbd3ed73427cc968
ye win <yewin.aung@zigway.co>
2022-01-15 14:43:38 +0700

    add react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,  
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
+import Home from './containers/Home';
+import Search from './containers/Search';
+import Detail from './containers/Detail';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Link to="/">Home</Link>
+      <Link to="/search">Search</Link>
+      <Link to="/detail">Detail</Link>
+
+      <Routes>
+        <Route exact path="/" element={<Home />} />                  
+        <Route path="/search" element={<Search />} />                  
+        <Route path="/detail" element={<Detail />} />                
+      </Routes>      
+    </Router>
   );
 }
 
--- src/containers/Detail/index.js
+++ src/containers/Detail/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Detail() {
     return (
         <div>
-            
+            detail
         </div>
     )
 }
